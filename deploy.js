3172b6fb5321b0b4b27f384f5f2963499d3714e6
Cristian Buta <cristianbuta@192-168-0-195.rdsnet.ro>
2021-12-03 10:40:43 +0200

    initial commit

---
+++ deploy.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3')
+
+const { abi, evm } = require('./compile')
+
+provider = new HDWalletProvider(
+  process.env.RINKEBY_MNEMONIC,
+  process.env.INFURA_RINKEBY_ENDPOINT,
+)
+
+const web3 = new Web3(provider)
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts()
+
+  console.log('Attempting to deploy from account', accounts[0])
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] })
+
+  console.log('Contract deployed to', result.options.address)
+  provider.engine.stop()
+}
+deploy()
